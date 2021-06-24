358a13806f54abdb8df62cd935bf28bf13d0987d
Ann <annie.wang50@yahoo.com>
2021-06-24 13:09:37 -0400

    style for login/signup

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,9 +1,11 @@
+
+import PageHeader from '../PageHeader/PageHeader'
 import './Login.css'
 
 export default function Login() {
   return (
     <div className="Login">
-      
+      <PageHeader sectionName='Login'/>
     </div>
   )
 }
--- frontend/src/components/SignUp/SignUp.js
+++ frontend/src/components/SignUp/SignUp.js
@@ -1,9 +1,11 @@
+
+import PageHeader from '../PageHeader/PageHeader'
 import './Signup.css'
 
 export default function Signup() {
   return (
     <div className="Signup">
-      
+      <PageHeader sectionName='Sign Up'/>
     </div>
   )
 }
