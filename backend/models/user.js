3d475d1344de641f319c33749fcb05a122a00188
Ann <annie.wang50@yahoo.com>
2021-06-25 13:14:44 -0400

    user and auth login/register post requests

---
+++ backend/models/user.js
@@ -0,0 +1,80 @@
+const db = require('../db')
+const bcrypt = require('bcrypt')
+const { BCRYPT_WORK_FACTOR } = require('../config')
+const { BadRequestError, UnauthorizedError } = require('../utils/errors')
+
+class User {
+  static makePublicUser(user) {
+    return {
+      id: user.id,
+      email: user.email,
+      isAdmin: user.is_admin,
+      createdAt: user.created_at,
+    }
+  }
+
+  static async login(creds) {
+    const reqFields = ["email", "password"]
+    reqFields.forEach(field => {
+      if (!creds.hasOwnProperty(field)) {
+        throw new BadRequestError(`Missing ${field} in request body.`)
+      }
+    })
+    const user = await User.fetchUserByEmail(creds.email)
+    if (user) {
+      // compare entered password with password in database
+      const isValid = await bcrypt.compare(creds.password, user.password)
+      if (isValid) {
+        return User.makePublicUser(user)
+      }
+    }
+
+    throw new UnauthorizedError("Invalid email/password")
+  }
+
+  static async register(creds) {
+    const reqFields = ["email", "password", "isAdmin"]
+    reqFields.forEach(field => {
+      if (!creds.hasOwnProperty(field)) {
+        throw new BadRequestError(`Missing ${field} in request body.`)
+      }
+    })
+
+    if (creds.email.indexOf("@") <= 0) {
+      throw new BadRequestError("Invalid email.")
+    }
+
+    const existingUser = await User.fetchUserByEmail(credentials.email)
+    if (existingUser) {
+      throw new BadRequestError(`A user already exists with email: ${credentials.email}`)
+    }
+
+    const hashedPassword = await bcrypt.hash(creds.password, BCRYPT_WORK_FACTOR)
+    const normalizedEmail = creds.email.toLowerCase()
+
+    // insert new user into database
+    const userResult = await db.query(
+      `INSERT INTO users(email, password, is_admin)
+       VALUES ($1, $2, $3)
+       RETURNING id, email, is_admin, created_at;
+      `, [normalizedEmail, hashedPassword, creds.isAdmin]
+    )
+
+    const user = userResult.rows[0]
+
+    return User.makePublicUser(user)
+  }
+
+
+  static async fetchUserByEmail(email) {
+    if (!email) {
+      throw new BadRequestError("No email provided")
+    }
+    const query = `SELECT * FROM users WHERE email = $1`
+    const result = await db.query(query, [email.toLowerCase()])
+    const user = result.rows[0]
+    return user
+  }
+}
+
+module.exports = User
--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,21 +1,36 @@
 
+import { useState } from 'react'
 import PageHeader from '../PageHeader/PageHeader'
 import './Login.css'
 
 export default function Login() {
+  const [errors, setErrors] = useState({})
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleOnInputChange = () => {
+
+  }
+
+  const handleOnSubmit = () => {
+
+  }
+  
   return (
     <div className="Login">
       <PageHeader sectionName='Login'/>
       <div className='form'>
         <div className='form-input'>
           <label htmlFor='email'>Email</label>
-          <input type='email' name='email' placeholder='user@codepath.org'/>
+          <input type='email' name='email' placeholder='user@codepath.org' value={form.email} onChange={handleOnInputChange}/>
         </div>
         <div className='form-input'>
           <label htmlFor='password'>Password</label>
-          <input type='password' name='password' placeholder='*********'/>
+          <input type='password' name='password' placeholder='*********' value={form.password} onChange={handleOnInputChange}/>
         </div>
-        <button className='login-btn'>Login</button>
+        <button className='login-btn' onClick={handleOnSubmit}>Login</button>
       </div>
     </div>
   )
